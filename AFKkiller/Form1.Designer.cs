1ab8ce3e41ff48ebb2923d413e70b5ae6ce5e464
TIm <Bauer.tim.schortens@gmail.com>
2021-06-11 10:15:25 +0200

    Projektdateien hinzufügen.

---
+++ AFKkiller/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace AFKkiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_process = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_process = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(13, 221);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(108, 25);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(13, 249);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(108, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btn_process
+            // 
+            this.btn_process.Location = new System.Drawing.Point(13, 192);
+            this.btn_process.Name = "btn_process";
+            this.btn_process.Size = new System.Drawing.Size(108, 23);
+            this.btn_process.TabIndex = 3;
+            this.btn_process.Text = "Find Process";
+            this.btn_process.UseVisualStyleBackColor = true;
+            this.btn_process.Click += new System.EventHandler(this.btn_process_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Process Name :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_process
+            // 
+            this.txt_process.Location = new System.Drawing.Point(13, 32);
+            this.txt_process.Name = "txt_process";
+            this.txt_process.Size = new System.Drawing.Size(100, 23);
+            this.txt_process.TabIndex = 5;
+            this.txt_process.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 284);
+            this.Controls.Add(this.txt_process);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_process);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_process;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_process;
+    }
+}
+
---
+++ AFKkiller/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AFKkiller
+{
+
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            String test = listBox1.SelectedItem.ToString();
+            
+            afk();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void afk()
+        {
+            SendKeys.SendWait("a");
+            SendKeys.SendWait("b");
+            SendKeys.SendWait("c");
+            SendKeys.SendWait("d");
+            SendKeys.SendWait("e");
+            SendKeys.SendWait("f");
+            SendKeys.SendWait("g");
+            SendKeys.SendWait("h");
+            SendKeys.SendWait("i");
+            SendKeys.SendWait("j");
+            SendKeys.SendWait("k");
+            SendKeys.SendWait("l");
+            SendKeys.SendWait("m");
+            SendKeys.SendWait("n");
+            SendKeys.SendWait("o");
+            SendKeys.SendWait("p");
+            SendKeys.SendWait("q");
+            SendKeys.SendWait("r");
+            SendKeys.SendWait("s");
+            SendKeys.SendWait("t");
+            SendKeys.SendWait("u");
+            SendKeys.SendWait("v");
+            SendKeys.SendWait("w");
+            SendKeys.SendWait("x");
+            SendKeys.SendWait("y");
+            SendKeys.SendWait("z");
+
+        }
+
+        private void btn_process_Click(object sender, EventArgs e)
+        {
+            string[] processes = Process.GetProcesses().
+                     Where(x => x.ProcessName.Contains(txt_process.Text)). // Take care about case-sensivity of comparsion
+                     Select(x =>x.Id+":"+ x.ProcessName + ".exe").
+                     ToArray();
+
+            listBox1.Items.AddRange(processes);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
